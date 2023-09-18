14f667c8c5ec1eaa52b415e757c4ce3fc8c56855
Ryan Alberto Lopes de Oliveira <106593931+Ryanloopes@users.noreply.github.com>
2023-09-17 22:06:21 -0300

    Add files via upload

---
+++ CSS/script.js
@@ -0,0 +1,56 @@
+var amoepoints = 0;
+
+// Função para atualizar o total de moedas na tela
+function updateCoinCount() {
+    var coinCountElement = document.getElementById('coinCount');
+    coinCountElement.textContent = amoepoints;
+}
+
+// Função para ganhar 1 amoepoint por clique no logista
+var logistaElement = document.querySelector('.perfil');
+logistaElement.addEventListener('click', function () {
+    amoepoints += 1; // Ganhe 1 amoepoint por clique
+    updateCoinCount();
+});
+
+// Função para comprar um item
+function buyItem(itemName, itemPrice) {
+    if (amoepoints >= itemPrice) {
+        amoepoints -= itemPrice;
+        updateCoinCount();
+
+        // Personalizar o link de e-mail com o título da mercadoria
+        var emailSubject = 'Compra de ' + itemName;
+        var emailRecipient = 'seu_email@seu_domínio.com'; // Substitua pelo seu endereço de e-mail
+        var emailBody = 'Olá, gostaria de confirmar a compra do item: ' + itemName;
+
+        var emailLink = 'mailto:' + emailRecipient + '?subject=' + encodeURIComponent(emailSubject) + '&body=' + encodeURIComponent(emailBody);
+        var emailElement = document.createElement('a');
+        emailElement.setAttribute('href', emailLink);
+        emailElement.style.display = 'none';
+        document.body.appendChild(emailElement);
+
+        // Clicar automaticamente no link para abrir o cliente de e-mail
+        emailElement.click();
+        document.body.removeChild(emailElement);
+
+        // Mostrar modal de sucesso de compra
+        showSuccessModal();
+    } else {
+        // Mostrar modal de "Saldo Insuficiente"
+        var modal = document.getElementById('insufficientModal');
+        modal.style.display = 'block';
+        setTimeout(function () {
+            modal.style.display = 'none';
+        }, 2000); // Ocultar o modal após 2 segundos
+    }
+}
+
+// Função para mostrar o modal de sucesso de compra
+function showSuccessModal() {
+    var successModal = document.getElementById('successModal');
+    successModal.style.display = 'block';
+    setTimeout(function () {
+        successModal.style.display = 'none';
+    }, 2000); // Ocultar o modal após 2 segundos
+}
